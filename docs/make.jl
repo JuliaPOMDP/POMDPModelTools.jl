93c989302a2fd6da46ce727071b6ed967f3c6bd5
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-07 15:03:48 -0700

    trying to get auto page discovery to work

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,6 @@
 using Documenter, POMDPModelTools
 
-makedocs(
-    # format=:html,
-    # sitename = "POMDPModelTools",
-    # pages = [
-    #          "About" => "index.md",
-    #          "Distributions"=>"distributions.md",
-    #          # "Distribution Tools"=>"distribution_tools.md",
-    #          # "Terminal States"=>"terminal.md",
-    #          # "Info Interface"=>"info.md",
-    #          # "Ordered Spaces"=>"ordered.md",
-    #          # "Convenience"=>"convenience.md",
-    #          # "Testing"=>"testing.md",
-    #          ]
-    )
+makedocs()
 
 deploydocs(
            deps = Deps.pip("mkdocs"),
