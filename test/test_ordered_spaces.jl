d7312aee7931be98984077a5aa4af45700587736
MaximeBouton <maxim.bouton@gmail.com>
2018-10-25 17:16:36 -0700

    remove default convenient implementations

--- test/test_ordered_spaces.jl
+++ test/test_ordered_spaces.jl
@@ -1,9 +1,14 @@
 let
     struct TigerPOMDPTestFixture <: POMDP{Bool, Int, Bool} end
-
+    POMDPs.states(::TigerPOMDPTestFixture) = (true, false)
+    POMDPs.stateindex(::TigerPOMDPTestFixture, s) = Int(s) + 1
+    POMDPs.n_states(::TigerPOMDPTestFixture) = 2 
     POMDPs.actions(m::TigerPOMDPTestFixture) = 0:2
     POMDPs.n_actions(m::TigerPOMDPTestFixture) = 3
     POMDPs.actionindex(m::TigerPOMDPTestFixture, s::Int) = s+1
+    POMDPs.observations(::TigerPOMDPTestFixture) = (true, false)
+    POMDPs.obsindex(::TigerPOMDPTestFixture, o) = Int(o) + 1
+    POMDPs.n_observations(::TigerPOMDPTestFixture) = 2
 
     pomdp = TigerPOMDPTestFixture()
 
@@ -15,12 +20,14 @@ end
 struct TM <: POMDP{Int, Int, Int} end
 POMDPs.states(::TM) = [1,3]
 POMDPs.n_states(::TM) = 2
+POMDPs.stateindex(::TM, s::Int) = s
 
 @test_throws ErrorException ordered_states(TM())
 
 struct TM2 <: POMDP{Int, Int, Int} end
 POMDPs.states(::TM2) = [1,3]
 POMDPs.n_states(::TM2) = 3
+POMDPs.stateindex(::TM2, s::Int) = s
 
 println("There should be a warning below:")
 ordered_states(TM2())
