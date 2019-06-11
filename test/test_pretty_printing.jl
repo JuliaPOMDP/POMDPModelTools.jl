e4eece133d49a2176793a397b51824626e5e9d0f
Zachary Sunberg <sunbergzach@gmail.com>
2019-06-11 11:06:17 -0700

    fixed bug that prints <everything else> even when not necessary

--- test/test_pretty_printing.jl
+++ test/test_pretty_printing.jl
@@ -6,3 +6,10 @@ iob = IOBuffer()
 io = IOContext(iob, :limit=>true, :displaysize=>(10, 7))
 showdistribution(io, d)
 @test String(take!(iob)) == "                     SparseCat{UnitRange{Int64},Array{Float64,1}} distribution\n                     ┌                                        ┐ \n                   1 ┤■ 0.02                                    \n                   2 ┤■ 0.02                                    \n                   3 ┤■ 0.02                                    \n   <everything else> ┤■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ 0.94   \n                     └                                        ┘ "
+
+# test that it doesn't print <everything else> when there are enough lines
+d = SparseCat([:a], 1.0)
+iob = IOBuffer()
+io = IOContext(iob, :limit=>true, :displaysize=>(10, 7))
+showdistribution(io, d)
+@test String(take!(iob)) == "      SparseCat{Array{Symbol,1},Float64} distribution\n      ┌                                        ┐ \n   :a ┤■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ 1.0   \n      └                                        ┘ "
