824a376eb4febffa23f3fba527143730fe6475e2
Zachary Sunberg <zachary.sunberg@colorado.edu>
2020-08-20 11:52:03 -0600

    trying to write implemented for obs_weight

--- src/obs_weight.jl
+++ src/obs_weight.jl
@@ -9,3 +9,21 @@ Return a weight proportional to the likelihood of receiving observation o from s
 This is a useful shortcut for particle filtering so that the observation distribution does not have to be represented.
 """
 obs_weight(p, s, a, sp, o) = pdf(observation(p, s, a, sp), o)
+
+function POMDPLinter.implemented(::typeof(obs_weight), argtypes::Type{<:Tuple})
+    in_other_module = false
+    try
+        m = which(obs_weight, argtypes)
+        if m.module != POMDPModelTools
+            in_other_module = true
+        end
+    end
+    if in_other_module
+        return true
+    else
+        if implemented(observation, Tuple{only(argtypes.parameters).parameters[1:end-1]...})
+            Base.return_types
+
+    end
+    return missing
+end
